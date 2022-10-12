b0f7d5faf6c10c62e42c6cd24e2f281a269536b0
dyerfire9 <canadianraza@yahoo.com>
2022-10-12 00:34:33 -0400

    adjusted descriptions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Interests from "./components/Interests";
 import Socials from "./components/Socials";
 import "./style.css";
 
+
 export default function App() {
   return (
     <div className="container">
